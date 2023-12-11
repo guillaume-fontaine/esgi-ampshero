e299b0a953b086e9b76c2a87feef750e76714af2
pjehan <pierre.jehan@gmail.com>
2023-12-11 14:06:10 +0100

    Formulaire de création d'un utilisateur

--- routes/users.js
+++ routes/users.js
@@ -6,9 +6,19 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/:username', (req, res) => {
+router.get('/profile/:username', (req, res) => {
   const {username} = req.params;
-  res.render('users/profile', {username: username})
+  res.render('users/profile', {username: username});
+});
+
+router.get('/create', (req, res) => res.render('users/form'));
+
+router.post('/', async (req, res) => {
+  const {db} = req.app.locals;
+  const {username, email, password} = req.body;
+  const user = await db.collection('users').insertOne({ username: username, email: email, password: password });
+  console.log(user);
+  res.redirect('/users/profile/' + username);
 });
 
 module.exports = router;

10dc8d368ae0fd56587a5bb90ec9a9d92bb09ce4
pjehan <pierre.jehan@gmail.com>
2023-12-11 11:09:43 +0100

    Création d'une page de profil d'un utilisateur

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/:username', (req, res) => {
+  const {username} = req.params;
+  res.render('users/profile', {username: username})
+});
+
 module.exports = router;

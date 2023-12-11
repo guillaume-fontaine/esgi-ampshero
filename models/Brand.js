0009814045917e2b2a74fdf84fcc7f6ebfe7a6fa
pjehan <pierre.jehan@gmail.com>
2023-12-11 16:12:57 +0100

    Création des models

---
+++ models/Brand.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const brandSchema = new Schema({
+  name: {type: String, required: true, unique: true},
+  logo: String,
+});
+
+const Brand = mongoose.model('Brand', brandSchema);
+
+module.exports = Brand;
